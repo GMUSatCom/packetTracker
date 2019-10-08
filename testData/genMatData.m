382173fadba093a3c41723a2046d4de13304af7f
apoulter <aaronmartinez2017@gmail.com>
2019-10-08 12:10:03 -0400

    added mat file for easy access

--- testData/genMatData.m
+++ testData/genMatData.m
@@ -1,10 +1,18 @@
 f = fopen('rawIQData', 'rb');
 iqData = fread (f, [2,Inf], 'float');
 fclose(f);
-cData1 = (iqData(1,:) + 1j * iqData(2,:)).';
-
+cData1 = (iqData(1,:) + 1j * iqData(2,:));
+cData1 = cData1(2e6:4e6);
 
 f = fopen('rawIQData2', 'rb');
 iqData = fread (f, [2,Inf], 'float');
 fclose(f);
-cData2 = (iqData(1,:) + 1j * iqData(2,:)).';
+cData2 = (iqData(1,:) + 1j * iqData(2,:));
+cData2 = cData2(2e6:4e6);
+
+
+
+
+
+
+
